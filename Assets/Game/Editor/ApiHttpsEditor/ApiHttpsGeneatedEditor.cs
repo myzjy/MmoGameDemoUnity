0a607015f16b6bad5d43c3784d4da550d9af3ef7
zjy <2910553808@qq.com>
2022-08-15 00:11:00 +0800

    文件

---
+++ Assets/Game/Editor/ApiHttpsEditor/ApiHttpsGeneatedEditor.cs
@@ -0,0 +1,10 @@
+﻿namespace GameEditor.ApiHttpsEditor
+{
+    /// <summary>
+    /// 编辑器下生成Api Http文件
+    /// </summary>
+    public class ApiHttpsGeneratedEditor:UnityEditor.Editor
+    {
+        // public static void Open
+    }
+}
