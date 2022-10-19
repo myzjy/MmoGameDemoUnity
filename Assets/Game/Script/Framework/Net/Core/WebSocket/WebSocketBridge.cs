511f3e5c14d876055999baa28f9bc1f45d49dfe2
zjy <2910553808@qq.com>
2022-10-20 00:10:25 +0800

    socket 继续完善

--- Assets/Game/Script/Framework/Net/Core/WebSocket/WebSocketBridge.cs
+++ Assets/Game/Script/Framework/Net/Core/WebSocket/WebSocketBridge.cs
@@ -1,38 +1,122 @@
 ﻿using System;
+using System.Collections.Generic;
+using AOT;
+using BestHTTP.JSON.LitJson;
 using BestHTTP.WebSocket;
+using BestHTTP.WebSocket.Frames;
+using Newtonsoft.Json;
+using UnityEngine;
+using ZJYFrameWork.Spring.Utils;
 
 namespace ZJYFrameWork.Net.Core.Websocket
 {
-    internal static class WebSocketBridge
+    public class WebSocketBridge
     {
-        /* Delegates */
-        /// <summary>
-        /// 打开回调
-        /// </summary>
-        public delegate void OnOpenCallback();
-
-        /// <summary>
-        /// 发送消息回调
-        /// </summary>
-        public delegate void OnMessageCallback(IntPtr msgPtr, int msgSize);
-
-        /// <summary>
-        /// 当出错了的回调
-        /// </summary>
-        public delegate void OnErrorCallback();
-
-        /// <summary>
-        /// 当socket关闭了的回调
-        /// </summary>
-        public delegate void OnCloseCallback();
-
         /// <summary>
         /// webSocket
         /// </summary>
-        private static WebSocket _webSocket;
-
-        public static void DelegateOnOpenEvent()
+        private WebSocket _webSocket = new WebSocket(null)
         {
+            OnOpen = null,
+            OnMessage = null,
+            OnError = null,
+            OnClosed = null
+        };
+
+        public WebsocketClient websocketClient;
+
+        public bool initialized = false;
+
+        /// <summary>
+        /// 当socket打开成功的回调
+        /// </summary>
+        /// <param name="callback"></param>
+        public void WebSocketSetOnOpen(OnWebSocketOpenDelegate callback)
+        {
+            _webSocket.OnOpen = callback;
+        }
+
+        /// <summary>
+        /// 当服务器发送json或者text相关内容时回调
+        /// </summary>
+        /// <param name="callback"></param>
+        public void WebSocketSetOnMessage(OnWebSocketMessageDelegate callback)
+        {
+            _webSocket.OnMessage = callback;
+        }
+
+        /// <summary>
+        /// 当服务器发送消息为二进制的时候回调
+        /// </summary>
+        /// <param name="callback"></param>
+        public void WebSocketSetOnBinary(OnWebSocketBinaryDelegate callback)
+        {
+            _webSocket.OnBinary = callback;
+        }
+
+        /// <summary>
+        /// socket报错时回调
+        /// </summary>
+        /// <param name="callback"></param>
+        public void WebSocketSetOnError(OnWebSocketErrorDelegate callback)
+        {
+            _webSocket.OnError = callback;
+        }
+
+        /// <summary>
+        /// socket关闭时回调
+        /// </summary>
+        /// <param name="callback"></param>
+        public void WebSocketSetOnClose(OnWebSocketClosedDelegate callback)
+        {
+            _webSocket.OnClosed = callback;
+        }
+
+        public void Initialize()
+        {
+            WebSocketSetOnOpen(DelegateOnOpenEvent);
+            initialized = true;
+        }
+
+        [MonoPInvokeCallback(typeof(OnWebSocketOpenDelegate))]
+        public void DelegateOnOpenEvent(WebSocket webSocket)
+        {
+            websocketClient.HandleOnOpen(webSocket);
+        }
+
+        [MonoPInvokeCallback(typeof(OnWebSocketMessageDelegate))]
+        public static void DelegateOnMessageEvent(WebSocket webSocket, string message)
+        {
+            try
+            {
+                //
+                if (webSocket.IsOpen)
+                {
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                throw;
+            }
+        }
+
+        [MonoPInvokeCallback(typeof(OnWebSocketBinaryDelegate))]
+        public static void DelegateOnBinaryEvent(WebSocket webSocket, byte[] data)
+        {
+            try
+            {
+                //
+                if (webSocket.IsOpen)
+                {
+                   
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                throw;
+            }
         }
     }
 }
--- Assets/Game/Script/Framework/Net/Core/WebSocket/WebsocketClient.cs
+++ Assets/Game/Script/Framework/Net/Core/WebSocket/WebsocketClient.cs
@@ -1,12 +1,84 @@
-﻿using BestHTTP.WebSocket;
+﻿using System.Collections.Generic;
+using BestHTTP.WebSocket;
+using Newtonsoft.Json;
+using ZJYFrameWork.Net.CsProtocol.Buffer;
+using ZJYFrameWork.Spring.Utils;
 
 namespace ZJYFrameWork.Net.Core.Websocket
 {
-    public class WebsocketClient
+    public class WebsocketClient : AbstractClient
     {
-        public void Start()
+        private string url;
+        private WebSocketBridge _webSocketBridge = new WebSocketBridge();
+
+        public WebsocketClient(string url)
         {
-            // WebSocket
+            this.url = url;
+        }
+
+        public override void Start()
+        {
+            if (!_webSocketBridge.initialized)
+            {
+            }
+        }
+
+        public override bool Connected()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Close()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ToConnectUrl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void SendMessagesBlocking(byte[] messages, int offset, int size)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool ReadMessageBlocking(out byte[] content)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 打开回调
+        /// </summary>
+        internal void HandleOnOpen(WebSocket ws)
+        {
+        }
+
+        internal void HandleOnMessage(byte[] content)
+        {
+            var json = StringUtils.BytesToString(content);
+          
+            var jsonDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            
+            
+            
+            jsonDict.TryGetValue("protocolId", out var protocolStr);
+            
+            if (protocolStr != null)
+            {
+                var protocolId = short.Parse(protocolStr);
+                jsonDict.TryGetValue("packet",out var packetString);
+                // var packetStr = 
+            }
+
+            // var byteBuffer = ByteBuffer.ValueOf();
+            // byteBuffer.WriteBytes(content);
+
+            // byteBuffer.ReadRawInt();
+            // var packet = ProtocolManager.Read(byteBuffer);
+            // // queue it
+            // receiveQueue.Enqueue(new Message(MessageType.Data, packet));
         }
     }
 }
