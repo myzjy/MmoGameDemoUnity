65a41a9a6053cf8983acf4c4761090b85f46c6b1
zjy <2910553808@qq.com>
2023-01-09 00:32:00 +0800

    修改

---
+++ Assets/Game/Hoftix/Script/UI/GameMain/GameMainMiddleUIDataPanel.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using ZJYFrameWork.Collection.Reference;
+
+namespace ZJYFrameWork.UISerializable
+{
+    public class GameMainMiddleRightUIPanel : IReference
+    {
+        /// <summary>
+        /// 迷宫
+        /// </summary>
+        private Button _migongBtnButton;
+
+        /// <summary>
+        /// PVE 按钮
+        /// </summary>
+        private Button _pveBtnButton;
+
+        /// <summary>
+        /// PVP按钮
+        /// </summary>
+        private Button _pvpBtnButton;
+
+        /// <summary>
+        /// 入侵按钮 pvp
+        /// </summary>
+        private Button _ruqinButton;
+
+        /// <summary>
+        ///  爬塔按钮
+        /// </summary>
+        private Button _wujinBtnButton;
+
+        public Button PveBtnButton
+        {
+            get => _pveBtnButton;
+            set => _pveBtnButton = value;
+        }
+
+        public Button RuqinButton
+        {
+            get => _ruqinButton;
+            set => _ruqinButton = value;
+        }
+
+        public Button WujinBtnButton
+        {
+            get => _wujinBtnButton;
+            set => _wujinBtnButton = value;
+        }
+
+        public Button PvpBtnButton
+        {
+            get => _pvpBtnButton;
+            set => _pvpBtnButton = value;
+        }
+
+        public Button MigongBtnButton
+        {
+            get => _migongBtnButton;
+            set => _migongBtnButton = value;
+        }
+
+        public void Clear()
+        {
+            _pveBtnButton = null;
+            _ruqinButton = null;
+            _wujinBtnButton = null;
+            _pvpBtnButton = null;
+            _migongBtnButton = null;
+        }
+
+        public static GameMainMiddleRightUIPanel ValueOf(UISerializableKeyObject serializableKeyObject)
+        {
+            var item = ReferenceCache.Acquire<GameMainMiddleRightUIPanel>();
+            item._pveBtnButton = serializableKeyObject.GetObjType<Button>("PVEBtn_Button");
+            item._ruqinButton = serializableKeyObject.GetObjType<Button>("ruqin_Button");
+            item._wujinBtnButton = serializableKeyObject.GetObjType<Button>("wujinBtn_Button");
+            item._pvpBtnButton = serializableKeyObject.GetObjType<Button>("pvpBtn_Button");
+            item._migongBtnButton = serializableKeyObject.GetObjType<Button>("migongBtn_Button");
+            return item;
+        }
+    }
+
+    public class GameMainMiddleDownRightUIPanel : IReference
+    {
+        /// <summary>
+        /// 编制
+        /// </summary>
+        private Button ArmsBtn;
+
+        /// <summary>
+        /// 背包
+        /// </summary>
+        private Button BagButton;
+
+        /// <summary>
+        /// 英雄
+        /// </summary>
+        private Button HeroBtn;
+
+        /// <summary>
+        ///  抽卡界面按钮
+        /// </summary>
+        private Button RecruitBtn;
+
+        /// <summary>
+        /// 技能
+        /// </summary>
+        private Button skillButton;
+
+        /// <summary>
+        /// 任务按钮
+        /// </summary>
+        private Button TaskButton;
+
+        /// <summary>
+        /// 科技按钮
+        /// </summary>
+        private Button UniversityBtn;
+
+        public void Clear()
+        {
+            TaskButton = null;
+            UniversityBtn = null;
+            RecruitBtn = null;
+            HeroBtn = null;
+            ArmsBtn = null;
+            skillButton = null;
+            BagButton = null;
+        }
+    }
+
+    /// <summary>
+    /// 包含
+    /// </summary>
+    public class GameMainMiddleUIDataPanel
+    {
+        /// <summary>
+        /// 中间部分right边按钮
+        /// </summary>
+        private GameMainMiddleRightUIPanel _gameMainMiddleRightUIPanel;
+
+        private UISerializableKeyObject downRight_UISerializableKeyObject;
+
+        /// <summary>
+        /// 福利按钮
+        /// </summary>
+        private Button FuLiBtn;
+
+        private GameMainMiddleDownRightUIPanel GameMainMiddleDownRightUIPanel;
+
+        /// <summary>
+        /// 生成角色
+        /// </summary>
+        private GameObject heroHead;
+
+        /// <summary>
+        /// 活动按钮
+        /// </summary>
+        private Button HuodongBtn;
+
+        private UISerializableKeyObject Right_UISerializableKeyObject;
+
+        /// <summary>
+        /// 商城按钮
+        /// </summary>
+        private Button storeButton;
+
+        public GameMainMiddleRightUIPanel GameMainMiddleRightUIPanel
+        {
+            get
+            {
+                try
+                {
+                    if (_gameMainMiddleRightUIPanel == null)
+                    {
+                        _gameMainMiddleRightUIPanel = GameMainMiddleRightUIPanel.ValueOf(Right_UISerializableKeyObject);
+                    }
+                }
+                catch (Exception e)
+                {
+#if UNITY_EDITOR || DEVELOP_BUILD && ENABLE_LOG
+
+#endif
+                    // Console.WriteLine(e);
+                    throw new NullReferenceException(e.Message);
+                }
+
+
+                return _gameMainMiddleRightUIPanel;
+            }
+        }
+
+        public void Init(UISerializableKeyObject uiSerializableKeyObject)
+        {
+            heroHead = uiSerializableKeyObject.GetObjType<GameObject>("heroHead");
+            storeButton = uiSerializableKeyObject.GetObjType<Button>("storeButton");
+            HuodongBtn = uiSerializableKeyObject.GetObjType<Button>("HuodongBtn");
+            FuLiBtn = uiSerializableKeyObject.GetObjType<Button>("FuLiBtn");
+            Right_UISerializableKeyObject =
+                uiSerializableKeyObject.GetObjType<UISerializableKeyObject>("Right_UISerializableKeyObject");
+            downRight_UISerializableKeyObject =
+                uiSerializableKeyObject.GetObjType<UISerializableKeyObject>("downRight_UISerializableKeyObject");
+        }
+    }
+}
--- Assets/Game/Script/Framework/UI/UIRootCS/UIRoot.cs
+++ Assets/Game/Script/Framework/UI/UIRootCS/UIRoot.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ZJYFrameWork.UISerializable.Framwork.UIRootCS
 {
@@ -8,39 +9,61 @@ namespace ZJYFrameWork.UISerializable.Framwork.UIRootCS
         /// 背景panel
         /// </summary>
         [SerializeField] private GameObject bgPanel;
+
+        [FormerlySerializedAs("BgTransformPanel")] [SerializeField]
+        private Transform bgTransformPanel;
+
+        [FormerlySerializedAs("UIPanel")] [SerializeField]
+        private GameObject uiPanel;
+
+        [FormerlySerializedAs("UITransformPanel")] [SerializeField]
+        private Transform uiTransformPanel;
+
+        [FormerlySerializedAs("UITopPanel")] [SerializeField]
+        private GameObject uiTopPanel;
+
+        [FormerlySerializedAs("UITopTransformPanel")] [SerializeField]
+        private Transform uiTopTransformPanel;
+
+        [FormerlySerializedAs("NoticeCanvasPanel")] [SerializeField]
+        private GameObject noticeCanvasPanel;
+
+        [FormerlySerializedAs("NoticeCanvasTransformPanel")] [SerializeField]
+        private Transform noticeCanvasTransformPanel;
+
+        [FormerlySerializedAs("ActiviesCanvasTransformPanel")] [SerializeField]
+        private Transform activiesCanvasTransformPanel;
+
+        [FormerlySerializedAs("ActiviesNoticeCanvasTransformPanel")] [SerializeField]
+        private Transform activiesNoticeCanvasTransformPanel;
+
         public GameObject GetBgPanel => bgPanel;
 
-        [SerializeField] private Transform BgTransformPanel;
-        public Transform GetBgTransformPanel => BgTransformPanel;
-        
-        [SerializeField] private GameObject UIPanel;
-        public GameObject GetUIPanel => UIPanel;
+        public Transform GetBgTransformPanel => bgTransformPanel;
 
-        [SerializeField] private Transform UITransformPanel;
-        public Transform GetUITransformPanel => UITransformPanel;
-        
-        [SerializeField] private GameObject UITopPanel;
-        public GameObject GetUITopPanel => UITopPanel;
+        public GameObject GetUIPanel => uiPanel;
 
-        [SerializeField] private Transform UITopTransformPanel;
-        public Transform GetUITopTransformPanel => UITopTransformPanel;
-        
-        [SerializeField] private GameObject NoticeCanvasPanel;
-        public GameObject GetNoticeCanvasPanel => NoticeCanvasPanel;
+        public Transform GetUITransformPanel => uiTransformPanel;
 
-        [SerializeField] private Transform NoticeCanvasTransformPanel;
-        public Transform GetNoticeCanvasTransformPanel => NoticeCanvasTransformPanel;
-        [SerializeField] private Transform ActiviesCanvasTransformPanel;
-        public Transform GetActivieseCanvasTransformPanel => ActiviesCanvasTransformPanel;
-        [SerializeField] private Transform ActiviesNoticeCanvasTransformPanel;
-        public Transform ActiviesGetNoticeCanvasTransformPanel => ActiviesNoticeCanvasTransformPanel;
+        public GameObject GetUITopPanel => uiTopPanel;
+
+        public Transform GetUITopTransformPanel => uiTopTransformPanel;
+
+        public GameObject GetNoticeCanvasPanel => noticeCanvasPanel;
+
+        public Transform GetNoticeCanvasTransformPanel => noticeCanvasTransformPanel;
+
+        public Transform GetActivieseCanvasTransformPanel => activiesCanvasTransformPanel;
+
+        public Transform ActiviesGetNoticeCanvasTransformPanel => activiesNoticeCanvasTransformPanel;
+
         public void SortOrder()
         {
             bgPanel.GetComponent<Canvas>().sortingOrder = 20;
-            UIPanel.GetComponent<Canvas>().sortingOrder = 50;
-            ActiviesCanvasTransformPanel.GetComponent<Canvas>().sortingOrder = 55;
-            UITopPanel.GetComponent<Canvas>().sortingOrder = 80;
-            NoticeCanvasPanel.GetComponent<Canvas>().sortingOrder = 100;
+            uiPanel.GetComponent<Canvas>().sortingOrder = 50;
+            activiesCanvasTransformPanel.GetComponent<Canvas>().sortingOrder = 55;
+            uiTopPanel.GetComponent<Canvas>().sortingOrder = 80;
+            noticeCanvasPanel.GetComponent<Canvas>().sortingOrder = 100;
         }
     }
 }
