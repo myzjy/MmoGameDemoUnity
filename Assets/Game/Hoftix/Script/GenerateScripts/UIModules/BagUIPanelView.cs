3d26f54e40e4c6edb1987b8610610c7be73dda09
zjy <2910553808@qq.com>
2023-01-18 00:00:19 +0800

    背包

---
+++ Assets/Game/Hoftix/Script/GenerateScripts/UIModules/BagUIPanelView.cs
@@ -0,0 +1,15 @@
+﻿using ZJYFrameWork.UISerializable.UIInitView;
+
+namespace ZJYFrameWork.UISerializable
+{
+    public class BagUIPanelView : UIViewInterface
+    {
+        public UnityEngine.GameObject EquipAndItemPanel = null;
+
+
+        public void Init(UIView _view)
+        {
+            EquipAndItemPanel = _view.GetObjType<UnityEngine.GameObject>("EquipAndItemPanel");
+        }
+    }
+}
