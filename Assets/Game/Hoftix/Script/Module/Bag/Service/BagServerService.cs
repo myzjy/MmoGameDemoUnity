e5bd95cdb4a88ecbd2507a7780a889d1e2759f50
zjy <2910553808@qq.com>
2023-01-19 00:43:58 +0800

    背包

---
+++ Assets/Game/Hoftix/Script/Module/Bag/Service/BagServerService.cs
@@ -0,0 +1,21 @@
+﻿using ZJYFrameWork.Net;
+using ZJYFrameWork.Spring.Core;
+
+namespace ZJYFrameWork.Hoftix.Script.Module.Bag.Service
+{
+    /// <summary>
+    /// 背包Service 具体实现
+    /// </summary>
+    [Bean]
+    public class BagServerService : IBagServerService
+    {
+        /// <summary>
+        /// 网络相关
+        /// </summary>
+        [Autowired] private INetManager _netManager;
+
+        public void GetBagServerData()
+        {
+        }
+    }
+}
--- Assets/Game/Hoftix/Script/Module/Bag/Service/IBagServerService.cs
+++ Assets/Game/Hoftix/Script/Module/Bag/Service/IBagServerService.cs
@@ -1,7 +1,13 @@
 ﻿namespace ZJYFrameWork.Hoftix.Script.Module.Bag.Service
 {
+    /// <summary>
+    /// 背包相关Service 接口
+    /// </summary>
     public interface IBagServerService
     {
-        
+        /// <summary>
+        /// 获取背包所有数据
+        /// </summary>
+        void GetBagServerData();
     }
 }
