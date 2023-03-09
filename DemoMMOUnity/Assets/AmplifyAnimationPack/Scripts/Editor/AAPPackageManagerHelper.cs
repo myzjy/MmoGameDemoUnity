45dc12276050a0445bfaee07976fb3cda022278b
zjy <2910553808@qq.com>
2023-03-09 19:25:17 +0800

    修改代码

--- DemoMMOUnity/Assets/AmplifyAnimationPack/Scripts/Editor/AAPPackageManagerHelper.cs
+++ DemoMMOUnity/Assets/AmplifyAnimationPack/Scripts/Editor/AAPPackageManagerHelper.cs
@@ -1,47 +1,57 @@
 // Amplify Animation Pack - Third-Person Character Controller
 // Copyright (c) Amplify Creations, Lda <info@amplify.pt>
 
-using System;
+#if UNITY_EDITOR
+
 using UnityEditor;
-using UnityEditor.EventSystems;
 using UnityEditor.PackageManager.Requests;
 using UnityEditor.PackageManager;
-using UnityEngine;
 
 
+// ReSharper disable once CheckNamespace
 namespace AmplifyAnimationPack
 {
-	public static class AAPPackageManagerHelper
-	{
-		public delegate void FinishedEventType();
-		public static FinishedEventType FinishedEvent;
-		private static AddRequest m_request;
-		public static void ImportPackage( string packageId )
-		{
-			if( m_request == null )
-			{
-				m_request = Client.Add( packageId );
-				EditorApplication.update += Progress;
-			}
-		}
+    public static class AAPPackageManagerHelper
+    {
+        public delegate void FinishedEventType();
 
-		static void Progress()
-		{
-			if( m_request.IsCompleted )
-			{
-				if( m_request.Status == StatusCode.Success )
-					Debug.Log( "Installed: " + m_request.Result.packageId );
-				else if( m_request.Status >= StatusCode.Failure )
-					Debug.Log( m_request.Error.message );
+        public static FinishedEventType FinishedEvent;
+        private static AddRequest m_request;
 
-				EditorApplication.update -= Progress;
+        public static void ImportPackage(string packageId)
+        {
+            if (m_request == null)
+            {
+                m_request = Client.Add(packageId);
+                EditorApplication.update += Progress;
+            }
+        }
 
-				m_request = null;
-				if( FinishedEvent != null )
-					FinishedEvent.Invoke();
-			}
-		}
+        static void Progress()
+        {
+            if (m_request.IsCompleted)
+            {
+                if (m_request.Status == StatusCode.Success)
+                {
+                    Debug.Log("Installed: " + m_request.Result.packageId);
+                }
+                else if (m_request.Status >= StatusCode.Failure)
+                {
+                    Debug.Log(m_request.Error.message);
+                }
 
-		public static bool CanStartImport { get { return m_request == null; } }
-	}
+                EditorApplication.update -= Progress;
+
+                m_request = null;
+                if (FinishedEvent != null)
+                    FinishedEvent.Invoke();
+            }
+        }
+
+        public static bool CanStartImport
+        {
+            get { return m_request == null; }
+        }
+    }
 }
+#endif
