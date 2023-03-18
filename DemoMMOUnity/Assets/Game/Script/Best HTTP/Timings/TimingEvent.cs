7fa206b5f05c24f6b286bcee03ae1c1378b002e8
zjy <2910553808@qq.com>
2023-03-18 15:21:31 +0800

    插件代码优化

--- DemoMMOUnity/Assets/Game/Script/Best HTTP/Timings/TimingEvent.cs
+++ DemoMMOUnity/Assets/Game/Script/Best HTTP/Timings/TimingEvent.cs
@@ -2,28 +2,29 @@ using System;
 
 namespace BestHTTP.Timings
 {
-    public struct TimingEvent : IEquatable<TimingEvent>
+    public readonly struct TimingEvent : IEquatable<TimingEvent>
     {
         public static readonly TimingEvent Empty = new TimingEvent(null, TimeSpan.Zero);
+
         /// <summary>
-        /// Name of the event
+        /// 事件名称
         /// </summary>
         public readonly string Name;
 
         /// <summary>
-        /// Duration of the event.
+        /// 事件的持续时间。
         /// </summary>
-        public readonly TimeSpan Duration;
+        private readonly TimeSpan _duration;
 
         /// <summary>
-        /// When the event occurred.
+        /// 事件发生的时间。
         /// </summary>
         public readonly DateTime When;
 
-        public TimingEvent(string name, TimeSpan duration)
+        private TimingEvent(string name, TimeSpan duration)
         {
             this.Name = name;
-            this.Duration = duration;
+            this._duration = duration;
             this.When = DateTime.Now;
         }
 
@@ -31,13 +32,15 @@ namespace BestHTTP.Timings
         {
             this.Name = name;
             this.When = when;
-            this.Duration = duration;
+            this._duration = duration;
         }
 
         public TimeSpan CalculateDuration(TimingEvent @event)
         {
             if (this.When < @event.When)
+            {
                 return @event.When - this.When;
+            }
 
             return this.When - @event.When;
         }
@@ -45,14 +48,16 @@ namespace BestHTTP.Timings
         public bool Equals(TimingEvent other)
         {
             return this.Name == other.Name &&
-                   this.Duration == other.Duration &&
+                   this._duration == other._duration &&
                    this.When == other.When;
         }
 
         public override bool Equals(object obj)
         {
-            if (obj is TimingEvent)
-                return this.Equals((TimingEvent)obj);
+            if (obj is TimingEvent @event)
+            {
+                return this.Equals(@event);
+            }
 
             return false;
         }
@@ -60,8 +65,8 @@ namespace BestHTTP.Timings
         public override int GetHashCode()
         {
             return (this.Name != null ? this.Name.GetHashCode() : 0) ^
-                this.Duration.GetHashCode() ^
-                this.When.GetHashCode();
+                   this._duration.GetHashCode() ^
+                   this.When.GetHashCode();
         }
 
         public static bool operator ==(TimingEvent lhs, TimingEvent rhs)
@@ -76,7 +81,7 @@ namespace BestHTTP.Timings
 
         public override string ToString()
         {
-            return string.Format("['{0}': {1}]", this.Name, this.Duration);
+            return $"['{this.Name}': {this._duration}]";
         }
     }
 }
