db39e6a7fe355702f61f3713998507d0c373c7fe
zjy <2910553808@qq.com>
2024-01-21 00:56:51 +0800

    表示 武器controller

---
+++ DemoMMOUnity/Assets/Game/Hoftix/Script/Module/Weapon/Controller/WeaponController.cs
@@ -0,0 +1,12 @@
+using ZJYFrameWork.Spring.Core;
+
+namespace ZJYFrameWork.Module.System.Controller
+{
+    /// <summary>
+    /// 武器的控制
+    /// </summary>
+    [Bean]
+    public class WeaponController
+    {
+    }
+}
