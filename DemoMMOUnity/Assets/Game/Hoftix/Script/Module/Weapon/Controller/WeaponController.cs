cf56d1f34f50cec8ecc19a5b0978e46ad3ef1bc7
zjy <2910553808@qq.com>
2024-01-22 00:51:12 +0800

    武器协议 完善

--- DemoMMOUnity/Assets/Game/Hoftix/Script/Module/Weapon/Controller/WeaponController.cs
+++ DemoMMOUnity/Assets/Game/Hoftix/Script/Module/Weapon/Controller/WeaponController.cs
@@ -1,3 +1,5 @@
+using ZJYFrameWork.Net.CsProtocol.Buffer.Protocol.Weapon;
+using ZJYFrameWork.Net.Dispatcher;
 using ZJYFrameWork.Spring.Core;
 
 namespace ZJYFrameWork.Module.System.Controller
@@ -8,5 +10,12 @@ namespace ZJYFrameWork.Module.System.Controller
     [Bean]
     public class WeaponController
     {
+        [PacketReceiver]
+        public void AtWeaponUsePlayerResponse(WeaponUsePlayerResponse response)
+        {
+#if UNITY_EDITOR||(DEVELOP_BUILD && ENABLE_LOG)
+            Debug.Log("调用到 at WeaponUsePlayerResponse");
+#endif
+        }
     }
 }
