2b8300f86487d8cc9cac586a4108cffde8add622
zjy <2910553808@qq.com>
2024-01-28 02:22:17 +0800

    背包UI界面第一版提交

--- DemoMMOUnity/Lua/application/app/ui/gameMain/gameMainView.lua
+++ DemoMMOUnity/Lua/application/app/ui/gameMain/gameMainView.lua
@@ -1,5 +1,38 @@
 ---@class GameMainView:UIView
 GameMainView = class("GameMainView", UIView)
+GameMainConfig = {
+    prefabName = "GameMainUIPanel",
+    --- 当前会生成在那一层
+    canvasType = UIConfigEnum.UICanvasType.UI,
+    sortType = UIConfigEnum.UISortType.First,
+    --- 当前 UI 交互事件 消息
+    eventNotification = {
+        --- 打开游戏主界面
+        OPEN_GAMEMAINPANEL_UI = "OPEN_GAMEMAINPANEL_UI",
+        --- 关闭 游戏主界面
+        CLOSE_GAMEMAINPANEL_UI = "CLOSE_GAMEMAINPANEL_UI",
+    }
+}
+require("application.app.ui.generateScripts.UIModules.GameMainUIPanelView")
 function GameMainView:OnLoad()
-    
+    self.UIConfig = {
+        Config = GameMainConfig,
+        viewPanel = GameMainUIPanelView,
+        initFunc = function()
+            printDebug("GameMainView:Init()")
+            GameMainView:OnInit()
+        end,
+        showFunc = function()
+            printDebug("GameMainView:showUI()-->showFunc")
+            GameMainView:OnShow()
+        end,
+        hideFunc = function() end,
+    }
+    self:Load(self.UIConfig)
+    self:LoadUI(self.UIConfig)
+    self:InstanceOrReuse()
+end
+
+function GameMainView:OnInit()
+    printDebug("call GameMainView Lua Script function to OnInit ....")
 end
--- DemoMMOUnity/Lua/application/app/ui/generateScripts/UIModules/BagUIPanelView.lua
+++ DemoMMOUnity/Lua/application/app/ui/generateScripts/UIModules/BagUIPanelView.lua
@@ -3,6 +3,12 @@ BagUIPanelView = class("BagUIPanelView")
 function BagUIPanelView:Init(view)
 	self._UIView = view:GetComponent("UIView")
 	self.equipmentItem_UISerializableKeyObject = self._UIView:GetObjType("equipmentItem_UISerializableKeyObject") or CS.ZJYFrameWork.UISerializable.UISerializableKeyObject
-	self.equipmentItem = self._UIView:GetObjType("equipmentItem") or CS.UnityEngine.GameObject
+	self.equipmentItem_CanvasGroup = self._UIView:GetObjType("equipmentItem_CanvasGroup") or CS.UnityEngine.CanvasGroup
+	self.topSelectTipsShowText = self._UIView:GetObjType("topSelectTipsShowText") or CS.UnityEngine.UI.Text
+	self.CloseBtn = self._UIView:GetObjType("CloseBtn") or CS.UnityEngine.UI.Button
+	self.equipmentButton = self._UIView:GetObjType("equipmentButton") or CS.UnityEngine.UI.Button
+	self.equipmentButtons_UISerializableKeyObject = self._UIView:GetObjType("equipmentButtons_UISerializableKeyObject") or CS.ZJYFrameWork.UISerializable.UISerializableKeyObject
+	self.WeaponInDetailItem_UISerializableKeyObject = self._UIView:GetObjType("WeaponInDetailItem_UISerializableKeyObject") or CS.ZJYFrameWork.UISerializable.UISerializableKeyObject
+	self.weaponGrid = self._UIView:GetObjType("weaponGrid") or CS.UnityEngine.GameObject
 end
 
