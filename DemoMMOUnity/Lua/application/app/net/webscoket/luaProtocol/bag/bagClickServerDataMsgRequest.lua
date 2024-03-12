534d122ac8f36d26a0b5ce8f5b396f9db68f3206
zjy <2910553808@qq.com>
2024-03-12 14:47:17 +0800

    背包

--- DemoMMOUnity/Lua/application/app/net/webscoket/luaProtocol/bag/bagClickServerDataMsgRequest.lua
+++ DemoMMOUnity/Lua/application/app/net/webscoket/luaProtocol/bag/bagClickServerDataMsgRequest.lua
@@ -1,11 +1,15 @@
----@class BagClickServerDataMsgRequet
-local BagClickServerDataMsgRequet = class("BagClickServerDataMsgRequet")
+---@class BagClickServerDataMsgRequest
+local BagClickServerDataMsgRequest = class("BagClickServerDataMsgRequet")
 
-function BagClickServerDataMsgRequet:ctor()
+function BagClickServerDataMsgRequest:ctor()
     ---@type string 点击的 名字
-    self.clickNameHanler = string.empty
+    self.clickNameHandler = string.empty
     ---@type number 根据 所选 type 返回协议不一样
     self.clickType = 0
+    ---@type number 根据不同的武器 道具id 服务器处理不一样
+    self.id=0
+    ---@type number 具体是那个玩家
+    self.masterUserId=0;
 end
 
-return BagClickServerDataMsgRequet
+return BagClickServerDataMsgRequest
