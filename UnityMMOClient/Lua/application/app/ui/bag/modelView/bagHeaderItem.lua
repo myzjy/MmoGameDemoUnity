4432a415bccb114a09943f5cb5dbe7c39818116a
zjy <2910553808@qq.com>
2024-03-20 21:00:53 +0800

    提交部分未被提交文件

---
+++ UnityMMOClient/Lua/application/app/ui/bag/modelView/bagHeaderItem.lua
@@ -0,0 +1,22 @@
+---@class BagHeaderItem:LuaUIObject
+local BagHeaderItem = class("BagHeaderItem", LuaUIObject())
+
+function BagHeaderItem:ctor(goRoot, config)
+    self.gameObject = goRoot
+    self.transform = goRoot.transform
+    self.config = config
+end
+
+--- 点击事件
+function BagHeaderItem:OnEnterFish()
+    if self.config.type == 1 then
+        self:OnClickWeaponBtnHandler()
+    end
+end
+
+function BagHeaderItem:OnClickWeaponBtnHandler()
+    -- 点击 背包 武器按钮
+    UIGameEvent.BagHeaderBtnWeaponHandler()
+end
+
+return BagHeaderItem
