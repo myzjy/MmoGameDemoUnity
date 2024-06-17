eb50b49dfa7bf5cb695780f11571db66ec9a1344
zjy <2910553808@qq.com>
2024-06-18 00:23:17 +0800

    添加新的log 脚本控制

---
+++ UnityMMOClient/Assets/FrostEngine/Runtime/Core/Log/GameFrameworkLog.cs
@@ -0,0 +1,245 @@
+using ZJYFrameWork.Spring.Utils;
+
+namespace FrostEngine
+{
+    /// <summary>
+    /// 游戏框架日志类。
+    /// </summary>
+    public static partial class GameFrameworkLog
+    {
+        private static ILogHelper s_LogHelper = new DefaultLogHelper();
+
+        /// <summary>
+        /// 设置游戏框架日志辅助器。
+        /// </summary>
+        /// <param name="logHelper">要设置的游戏框架日志辅助器。</param>
+        public static void SetLogHelper(ILogHelper logHelper)
+        {
+            s_LogHelper = logHelper;
+        }
+
+        /// <summary>
+        /// 打印调试级别日志，用于记录调试类日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Debug(object message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Debug, message);
+        }
+
+        /// <summary>
+        /// 打印调试级别日志，用于记录调试类日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Debug(string message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Debug, message);
+        }
+
+        /// <summary>
+        /// 打印调试级别日志，用于记录调试类日志信息。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        public static void Debug(string format,params object[] arg)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Debug, StringUtils.Format(format, arg));
+        }
+        
+
+        /// <summary>
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Info(object message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Info, message);
+        }
+
+        /// <summary>
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Info(string message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Info, message);
+        }
+
+        /// <summary>
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        public static void Info<T>(string format, T arg)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Info, StringUtils.Format(format, arg));
+        }
+        
+        /// <summary>
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Warning(object message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Warning, message);
+        }
+
+        /// <summary>
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Warning(string message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Warning, message);
+        }
+
+        /// <summary>
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        public static void Warning(string format,params object[] arg)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Warning, StringUtils.Format(format, arg));
+        }
+        
+
+        /// <summary>
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Error(object message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Error, message);
+        }
+
+        /// <summary>
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Error(string message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Error, message);
+        }
+
+        /// <summary>
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        public static void Error(string format,params object[] arg)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Error, StringUtils.Format(format, arg));
+        }
+
+
+        /// <summary>
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Fatal(object message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Fatal, message);
+        }
+
+        /// <summary>
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        public static void Fatal(string message)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Fatal, message);
+        }
+
+        /// <summary>
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        public static void Fatal(string format, params object[] arg)
+        {
+            if (s_LogHelper == null)
+            {
+                return;
+            }
+
+            s_LogHelper.Log(GameFrameworkLogLevel.Fatal, StringUtils.Format(format, arg));
+        }
+ 
+    }
+}
---
+++ UnityMMOClient/Assets/FrostEngine/Runtime/Core/Log/Log.cs
@@ -0,0 +1,276 @@
+using System.Diagnostics;
+using ZJYFrameWork.Spring.Utils;
+
+namespace FrostEngine
+{
+    public class FrostLog
+
+    {
+        /// <summary>
+        /// 打印调试级别日志，用于记录调试类日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_DEBUG_LOG 或 ENABLE_DEBUG_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_DEBUG_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        public static void Debug(object message)
+        {
+            GameFrameworkLog.Debug(message);
+        }
+
+        /// <summary>
+        /// 打印调试级别日志，用于记录调试类日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_DEBUG_LOG 或 ENABLE_DEBUG_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_DEBUG_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        public static void Debug(string message)
+        {
+            GameFrameworkLog.Debug(message);
+        }
+
+        /// <summary>
+        /// 打印调试级别日志，用于记录调试类日志信息。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_DEBUG_LOG 或 ENABLE_DEBUG_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_DEBUG_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        public static void Debug(string format, object[] arg)
+        {
+            GameFrameworkLog.Debug(format, arg);
+        }
+        
+        /// <summary>
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_INFO_LOG、ENABLE_DEBUG_AND_ABOVE_LOG 或 ENABLE_INFO_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_INFO_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        public static void Info(object message)
+        {
+            GameFrameworkLog.Info(message);
+        }
+
+        /// <summary>
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_INFO_LOG、ENABLE_DEBUG_AND_ABOVE_LOG 或 ENABLE_INFO_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_INFO_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        public static void Info(string message)
+        {
+            GameFrameworkLog.Info(message);
+        }
+
+        /// <summary>
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_INFO_LOG、ENABLE_DEBUG_AND_ABOVE_LOG 或 ENABLE_INFO_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_INFO_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        public static void Info(string format,params object[] arg)
+        {
+            GameFrameworkLog.Info(format, arg);
+        }
+
+        /// <summary>
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_WARNING_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG 或 ENABLE_WARNING_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_WARNING_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        public static void Warning(object message)
+        {
+            GameFrameworkLog.Warning(message);
+        }
+
+        /// <summary>
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_WARNING_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG 或 ENABLE_WARNING_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_WARNING_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        public static void Warning(string message)
+        {
+            GameFrameworkLog.Warning(message);
+        }
+
+        /// <summary>
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_WARNING_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG 或 ENABLE_WARNING_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_WARNING_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        public static void Warning(string format,params object[] arg)
+        {
+            GameFrameworkLog.Warning(format, arg);
+        }
+        
+
+        /// <summary>
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_ERROR_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG、ENABLE_WARNING_AND_ABOVE_LOG 或 ENABLE_ERROR_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_ERROR_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
+        public static void Error(object message)
+        {
+            GameFrameworkLog.Error(message);
+        }
+
+        /// <summary>
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_ERROR_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG、ENABLE_WARNING_AND_ABOVE_LOG 或 ENABLE_ERROR_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_ERROR_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
+        public static void Error(string message)
+        {
+            GameFrameworkLog.Error(message);
+        }
+
+        /// <summary>
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_ERROR_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG、ENABLE_WARNING_AND_ABOVE_LOG 或 ENABLE_ERROR_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_ERROR_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
+        public static void Error(string format,params object[] arg)
+        {
+            GameFrameworkLog.Error(format, arg);
+        }
+
+        /// <summary>
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_FATAL_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG、ENABLE_WARNING_AND_ABOVE_LOG、ENABLE_ERROR_AND_ABOVE_LOG 或 ENABLE_FATAL_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_FATAL_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_FATAL_AND_ABOVE_LOG")]
+        public static void Fatal(object message)
+        {
+            GameFrameworkLog.Fatal(message);
+        }
+
+        /// <summary>
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// </summary>
+        /// <param name="message">日志内容。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_FATAL_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG、ENABLE_WARNING_AND_ABOVE_LOG、ENABLE_ERROR_AND_ABOVE_LOG 或 ENABLE_FATAL_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_FATAL_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_FATAL_AND_ABOVE_LOG")]
+        public static void Fatal(string message)
+        {
+            GameFrameworkLog.Fatal(message);
+        }
+
+        /// <summary>
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// </summary>
+        /// <typeparam name="T">日志参数的类型。</typeparam>
+        /// <param name="format">日志格式。</param>
+        /// <param name="arg">日志参数。</param>
+        /// <remarks>仅在带有 ENABLE_LOG、ENABLE_FATAL_LOG、ENABLE_DEBUG_AND_ABOVE_LOG、ENABLE_INFO_AND_ABOVE_LOG、ENABLE_WARNING_AND_ABOVE_LOG、ENABLE_ERROR_AND_ABOVE_LOG 或 ENABLE_FATAL_AND_ABOVE_LOG 预编译选项时生效。</remarks>
+        [Conditional("ENABLE_LOG")]
+        [Conditional("ENABLE_FATAL_LOG")]
+        [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
+        [Conditional("ENABLE_FATAL_AND_ABOVE_LOG")]
+        public static void Fatal(string format, params object[] arg)
+        {
+            GameFrameworkLog.Fatal(format, arg);
+        }
+
+
+        /// <summary>
+        /// 断言严重错误级别日志，建议在发生严重错误。
+        /// </summary>
+        /// <param name="condition">条件。</param>
+        [Conditional("ENABLE_LOG")]
+        public static void Assert(bool condition)
+        {
+            if (!condition)
+            {
+                string message = StringUtils.Format("{0}\n{1}", "Assert Failed", System.Environment.StackTrace);
+                Fatal(message);
+            }
+        }
+
+        /// <summary>
+        /// 断言严重错误级别日志，建议在发生严重错误。
+        /// </summary>
+        /// <param name="condition">条件。</param>
+        /// <param name="retStr">断言输出字符串。</param>
+        [Conditional("ENABLE_LOG")]
+        public static void Assert(bool condition, string retStr)
+        {
+            if (!condition)
+            {
+                string message =
+                   StringUtils.Format("{0}\n{1}", "Assert Failed" + retStr, System.Environment.StackTrace);
+                Fatal(message);
+            }
+        }
+    }
+}
