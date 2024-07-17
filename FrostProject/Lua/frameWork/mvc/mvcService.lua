191cae8dc7fba75892bb5605df5dfa7a949d8ddf
zjy <2910553808@qq.com>
2024-07-18 00:13:56 +0800

    lua mvcService 文件初始创建

---
+++ FrostProject/Lua/frameWork/mvc/mvcService.lua
@@ -0,0 +1,11 @@
+--[[
+    author : zhangjingyi
+
+    作用 ： mvc 服务
+--]]
+
+---@class MVCService:ServiceBase
+local MVCService = Class("MVCService", ClassLibraryMap.ServiceBase)
+
+
+return MVCService
